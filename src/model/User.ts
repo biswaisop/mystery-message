4cf6e7349fa4b660c2cd3e3e550986b794ca2a54
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2024-11-27 01:55:00 +0530

    SCHEMA DEVELEOPMENT

--- src/model/User.ts
+++ src/model/User.ts
@@ -28,3 +28,34 @@ export interface User extends Document {
     isAcceptingMessage: boolean
 
 }
+
+const userschema: Schema<User> = new Schema({
+    userName: {
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true
+
+    },
+    email: {
+        type: String,
+        required: [true, "email is required"],
+        unique: true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "please use a valid email validation"]
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"],
+        minlength: 6
+    },
+    verifyCode: {
+        type: String
+    },
+    verifyCodeExpiry: {
+        type: Date
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true
+    }
+})
